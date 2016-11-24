043151a74d92808ed32883369aef25c963fa30f3
intracer <intracer@gmail.com>
2016-11-24 14:20:38 +0200

    initial commit

---
+++ src/main/java/org/wikipedia/uk/App.java
@@ -0,0 +1,13 @@
+package org.wikipedia.uk;
+
+/**
+ * Hello world!
+ *
+ */
+public class App 
+{
+    public static void main( String[] args )
+    {
+        System.out.println( "Hello World!" );
+    }
+}
